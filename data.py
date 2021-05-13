8db0680f99daf8db61a5bc5694409fb691b9fc56
Marta2399 <marta_-_martinez@hotmail.com>
2021-05-13 02:16:08 -0500

    Agregar pips compu

--- data.py
+++ data.py
@@ -52,4 +52,62 @@ def f_import_pmi():
 
 
 
+def f_import_precios():
+
+    gbp_usd_2019 = pd.read_csv("files/DAT_MT_GBPUSD_M1_2019.csv")
+    gbp_usd_2020 = pd.read_csv("files/DAT_MT_GBPUSD_M1_2020.csv")
+    gbp_usd_2021_e = pd.read_excel("files/DAT_XLSX_GBPUSD_M1_202101.xlsx")
+    gbp_usd_2021_f = pd.read_excel("files/DAT_XLSX_GBPUSD_M1_202102.xlsx")
+    gbp_usd_2021_m = pd.read_excel("files/DAT_XLSX_GBPUSD_M1_202103.xlsx")
+    gbp_usd_2021_a = pd.read_excel("files/DAT_XLSX_GBPUSD_M1_202104.xlsx")
+    
+    temp = [gbp_usd_2019["Fecha"][i].replace(".","-") for i in range(0, len(gbp_usd_2019["Fecha"]))]
+    fecha_2019 = [datetime.datetime.strptime(temp[i], "%Y-%m-%d") for i in range(0, len(temp))]
+    
+    hours_2019 = []
+    for i in gbp_usd_2019["Hora"]:
+        if isinstance(i,datetime.time):
+            hours_2019.append(i)
+        else:
+            hours_2019.append(datetime.datetime.strptime(i.strip(), '%H:%M').time())
+            
+    gbp_usd_2019["Fecha"] = fecha_2019
+    gbp_usd_2019["Hora"] = hours_2019
+    
+    temp = [gbp_usd_2020["Fecha"][i].replace(".","-") for i in range(0, len(gbp_usd_2020["Fecha"]))]
+    fecha_2020 = [datetime.datetime.strptime(temp[i], "%Y-%m-%d") for i in range(0, len(temp))]
+    
+    hours_2020 = []
+    for i in gbp_usd_2020["Hora"]:
+        if isinstance(i,datetime.time):
+            hours_2020.append(i)
+        else:
+            hours_2020.append(datetime.datetime.strptime(i.strip(), '%H:%M').time())
+            
+    gbp_usd_2020["Fecha"] = fecha_2020
+    gbp_usd_2020["Hora"] = hours_2020
+    
+    fechayhora_2019 = [datetime.datetime.combine(gbp_usd_2019["Fecha"][i], gbp_usd_2019["Hora"][i]) for i in range(0, len(gbp_usd_2019["Fecha"]))]
+    gbp_usd_2019["Fecha"] = fechayhora_2019
+    gbp_usd_2019.drop(["Hora"], axis=1, inplace=True)
+    gbp_usd_2019.rename(columns={"Fecha": "Fecha y hora"}, inplace=True)
+    
+    fechayhora_2020 = [datetime.datetime.combine(gbp_usd_2020["Fecha"][i], gbp_usd_2020["Hora"][i]) for i in range(0, len(gbp_usd_2020["Fecha"]))]
+    gbp_usd_2020["Fecha"] = fechayhora_2020
+    gbp_usd_2020.drop(["Hora"], axis=1, inplace=True)
+    gbp_usd_2020.rename(columns={"Fecha": "Fecha y hora"}, inplace=True)
+    
+    frames = [gbp_usd_2019, gbp_usd_2020, gbp_usd_2021_e, gbp_usd_2021_f, gbp_usd_2021_m, gbp_usd_2021_a]
+    precios_gbp_usd = pd.concat(frames)
+    precios_gbp_usd.set_index("Fecha y hora", inplace=True)
+
+    return precios_gbp_usd
+
+
+
+
+
+
+
+
 
--- functions.py
+++ functions.py
@@ -19,7 +19,158 @@ import statsmodels.api as sm
 from pylab import rcParams
 from scipy.stats import shapiro
 from statsmodels.tsa.stattools import adfuller
+from datetime import timedelta
+import datetime
 
+
+
+def escenarios_tabla():
+    return pd.DataFrame(
+        {
+            'Escenario': [
+                'A','B','C','D'
+            ],
+            'Regla': [
+                'Actual >= Consensus >= Previous','Actual >= Consensus < Previous',
+                'Actual < Consensus >= Previous',
+                'Actual <Consensus < Previous'
+            ],
+            'Posicion': ['venta','venta','compra','compra'
+            ],
+            'Take profit (pips)': [
+                20,20,10,10
+            ],
+            'Stop loss (pips)': [
+                10,10,5,5
+            ],
+            'Capital (USD)': [
+                1000,900,800,800
+            ]
+
+        }
+    ).set_index('Escenario')
+
+
+def f_escenario(data):
+
+    data['Escenario'] = None
+
+    for i in range(len(data)):
+        if data.iloc[i, 0] >= data.iloc[i, 1] and data.iloc[i, 1] >= data.iloc[i, 2]:
+            data.iloc[i, 3] = 'A'
+
+        elif data.iloc[i, 0] >= data.iloc[i, 1] and data.iloc[i, 1] < data.iloc[i,2]:
+            data.iloc[i, 3] = 'B'
+
+        elif data.iloc[i, 0] < data.iloc[i, 1] and data.iloc[i, 1] >= data.iloc[i,2]:
+            data.iloc[i, 3] = 'C'
+
+        elif data.iloc[i, 0] < data.iloc[i, 1] and data.iloc[i, 1] < data.iloc[i,2]:
+            data.iloc[i, 3] = 'D'
+
+    return data
+
+
+def operacion(p_inicial, p_final, p_max, p_min, tipo, t_profit, s_loss, fecha_hora):
+
+    if tipo=='compra':
+        tp = p_inicial + t_profit/10000
+        sl = p_inicial - s_loss/10000
+    else:
+        tp = p_inicial - t_profit / 10000
+        sl = p_inicial + s_loss / 10000
+
+
+    table = pd.DataFrame(
+        {'Concepto':['DateTime', 'Ventana', 'Precio inicial','Precio Final', 'Precio Max', 'Precio Min',
+                     'Stop loss', 'Take profit', 'Volumen','Capital', 'Posicion'],
+         'Valor':[fecha_hora, '30 min', p_inicial, p_final, p_max, p_min,
+                  sl, tp, int(1000/p_inicial),'1000 USD', tipo],
+        }
+    )
+
+    return table
+
+
+def f_metricas(indicador,precios_gbp_usd):    
+    date_time = []
+    for i in indicador.index:
+        date_time.append(datetime.datetime.fromtimestamp(datetime.datetime.timestamp(i)))
+
+    tiempos = []
+    for i in date_time:
+        for j in range(1,62):
+            tiempos.append(i - datetime.timedelta(minutes=31) + datetime.timedelta(minutes=j))
+
+    df_filtrado_por_indicador = pd.concat([precios_gbp_usd[precios_gbp_usd.index == str(i)] for i in tiempos])
+
+    direccion = []
+    iterador_t_0 = list(range(-31, len(df_filtrado_por_indicador), 61))
+    iterador_t_0.pop(0);
+    iterador_t_30 = list(range(-1, len(df_filtrado_por_indicador), 61))
+    iterador_t_30.pop(0);
+    
+    #(Dirección) Close (t_30) - Open(t_0)
+
+    for i in iterador_t_0:  
+        t_0 = df_filtrado_por_indicador.iloc[i, 0]
+        for j in iterador_t_30:
+            t_30 = df_filtrado_por_indicador.iloc[j, 3]
+        direccion.append(t_30 - t_0)
+    
+    uno_y_menos_uno = []
+    
+    for i in direccion:
+        if i > 0:
+            uno_y_menos_uno.append(1)
+        if i < 0:
+            uno_y_menos_uno.append(-1)
+
+    indicador["Direccion"] = uno_y_menos_uno
+    
+    #(Pips Alcistas) High(t_0 : t_30) – Open(t_0)
+    
+    high_hasta_t_30 = []
+    for i in iterador_t_0: 
+        high_hasta_t_30.append(max(df_filtrado_por_indicador.iloc[i: i + 31,1]))
+
+    pips_alcistas = []
+    for i in iterador_t_0:
+        t_0 = df_filtrado_por_indicador.iloc[i, 0]
+        for j in high_hasta_t_30:
+            t_max = j
+        pips_alcistas.append(t_max - t_0)
+        
+    indicador["Pips Alcistas"] = pips_alcistas
+    
+    #(Pips Bajistas) Open(t_0) – Low(t_0 : t_30) 
+    
+    low_hasta_t_30 = []
+    for i in iterador_t_0: 
+        low_hasta_t_30.append(min(df_filtrado_por_indicador.iloc[i: i + 31,1]))
+
+    pips_bajistas = []
+    for i in iterador_t_0:
+        t_0 = df_filtrado_por_indicador.iloc[i, 0]
+        for j in low_hasta_t_30:
+            t_min = j
+        pips_bajistas.append(t_0 - t_min)
+        
+    indicador["Pips Bajistas"] = pips_bajistas
+    
+    #(Volatilidad) High(t_-30 : t_30) ,  - mínimo low (t_-30:t_30)
+    
+    high_hasta_t_30 = []
+    for i in iterador_t_0: 
+        high_hasta_t_30.append(max(df_filtrado_por_indicador.iloc[i - 30: i + 31, 1]))
+    
+    low_hasta_t_30 = []
+    for i in iterador_t_0: 
+        low_hasta_t_30.append(min(df_filtrado_por_indicador.iloc[i - 30: i + 31,1]))
+
+    indicador["Volatilidad"] = [high_hasta_t_30[i] - low_hasta_t_30[i] for i in range(0, len(high_hasta_t_30))]
+     
+    return indicador
 
 
 
